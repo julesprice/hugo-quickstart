8fe7fe8c88877b733ff1bac9898fbd06acdf68e9
import-workflow <noreply@users.noreply.github.com>
2023-11-21 19:45:16 +0000

    import calls hugo workflow

--- script/import.sh
+++ script/import.sh
@@ -12,18 +12,18 @@ git config --global init.defaultBranch main
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# DATAPATH=$SCRIPT_DIR/../data/episode
-# python $SCRIPT_DIR/import.py \
-#     --spotify 'https://anchor.fm/s/822ba20/podcast/rss' \
-#     --youtube 'https://www.youtube.com/feeds/videos.xml?channel_id=UCTUcatGD6xu4tAcxG-1D4Bg' \
-#     --output "$DATAPATH"
-# git add "$DATAPATH"
+DATAPATH=$SCRIPT_DIR/../data/episode
+python $SCRIPT_DIR/import.py \
+    --spotify 'https://anchor.fm/s/822ba20/podcast/rss' \
+    --youtube 'https://www.youtube.com/feeds/videos.xml?channel_id=UCTUcatGD6xu4tAcxG-1D4Bg' \
+    --output "$DATAPATH"
+git add "$DATAPATH"
 
-# SITEPATH=$SCRIPT_DIR/../content/episodes
-# python $SCRIPT_DIR/createpages.py \
-#     --input "$DATAPATH" \
-#     --output "$SITEPATH"
-# git add "$SITEPATH"
+SITEPATH=$SCRIPT_DIR/../content/episodes
+python $SCRIPT_DIR/createpages.py \
+    --input "$DATAPATH" \
+    --output "$SITEPATH"
+git add "$SITEPATH"
 
 if git diff --staged --quiet
 then
