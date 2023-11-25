33090268b85ed1fbf012cf418954ce10ba532561
import-workflow <noreply@users.noreply.github.com>
2023-11-25 13:32:50 +0000

    env.sh: Configure git.  Make it re-runnable

--- script/env.sh
+++ script/env.sh
@@ -2,7 +2,12 @@
 #set -o xtrace
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"
 cd ..
-python -m venv .venv
-source .venv/Scripts/activate
-python -m pip install --upgrade pip
-python -m pip install -r script/requirements.txt
+ if [ ! -d .venv ]; then
+    python -m venv .venv
+    source .venv/Scripts/activate
+    python -m pip install --upgrade pip
+    python -m pip install -r script/requirements.txt
+fi
+git config --global init.defaultBranch main
+git config --global credential.helper wincred
+git config pull.rebase false
